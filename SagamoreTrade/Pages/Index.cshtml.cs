aeb41dc33d746fa42179f96ef427c7a106bd77f9
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-02-20 16:01:21 +0300

    dd

--- SagamoreTrade/Pages/Index.cshtml.cs
+++ SagamoreTrade/Pages/Index.cshtml.cs
@@ -1,24 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace SagamoreTrade.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly IConfiguration Configuration;
-        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            Configuration = configuration;
         }
 
         public void OnGet()
         {
-            ViewData["ChatId"] = Configuration["TelegramConfiguration:ChatId"];
-            ViewData["Test"] = Configuration["Test"];
         }
     }
 }
