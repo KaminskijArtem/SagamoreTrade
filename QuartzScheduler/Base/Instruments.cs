c036e153647cb5289649e53a1d9aa563bce9b9fc
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-02-25 16:43:32 +0300

    instruments added

--- QuartzScheduler/Base/Instruments.cs
+++ QuartzScheduler/Base/Instruments.cs
@@ -9,7 +9,10 @@ namespace QuartzScheduler.Base
         public static List<string> symbols = new List<string>
         { "BTC/USD" ,"ETH/USD" ,"XRP/USD" ,"LTC/USD", "DOGE/USD", "BCH/USD", "BTC/EUR", "ETH/EUR", "UNI/USD", "ETH/BTC",
             "XRP/EUR", "COMP/USD", "LINK/USD", "XRP/BTC", "BCH/BTC", "BTC/RUB", "LTC/BTC", "BCH/EUR", "ETH/RUB", "LTC/EUR",
-            "XRP/RUB", "XRP/BYN", "BTC/BYN", "ETH/BYN", "LTC/BYN", "LTC/RUB", "US500", "VIX", "US30", "DE30", "US100"
+            "XRP/RUB", "XRP/BYN", "BTC/BYN", "ETH/BYN", "LTC/BYN", "LTC/RUB",
+            "US500", "VIX", "US30", "DE30", "US100",
+            "Gold", "Silver", "Oil - Brent", "Oil - Crude", "Natural Gas", "Copper", "Platinum", "Aluminum",
+            "AAPL"
         };
 
         public static List<string> inPositionSymbols = new List<string>
