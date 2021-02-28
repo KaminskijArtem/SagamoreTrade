d20dac359ed35138273fdc34ffd02537138a3415
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-02-28 21:13:39 +0300

    ff

--- QuartzScheduler/Base/Instruments.cs
+++ QuartzScheduler/Base/Instruments.cs
@@ -9,13 +9,13 @@ namespace QuartzScheduler.Base
         public static List<string> symbols = new List<string>
         {   "BTC/USD" ,"BTC/EUR" ,"ETH/USD" , "ETH/EUR", "LTC/USD",  "BCH/USD", "ETH/BTC",
             //"LTC/EUR", "BCH/EUR",
-            //"US500", "VIX", "US30", "DE30", "US100", "DAX", "IT40", "SP35", "CN50", "NL25", 
+            //"US500", "US30", "US100", 
             //"Gold", "Copper"
         };
 
         public static List<string> inPositionSymbols = new List<string>
         {
-            "ETH/BTC"
+            
         };
     }
 }
