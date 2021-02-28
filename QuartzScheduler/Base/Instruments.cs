a024dbed88685a2566ae561b2a96e29e043333bd
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-02-28 20:59:10 +0300

    ff

--- QuartzScheduler/Base/Instruments.cs
+++ QuartzScheduler/Base/Instruments.cs
@@ -7,8 +7,10 @@ namespace QuartzScheduler.Base
     public class GlobalValues
     {
         public static List<string> symbols = new List<string>
-        {   "BTC/USD" ,"BTC/EUR" ,"ETH/USD" , "ETH/EUR", "LTC/USD", "LTC/EUR", "BCH/USD", "BCH/EUR", "ETH/BTC",
-            //"US500", "VIX", "US30", "DE30", "US100", "DAX", "IT40", "SP35", "CN50", "NL25", "Gold", "Copper"
+        {   "BTC/USD" ,"BTC/EUR" ,"ETH/USD" , "ETH/EUR", "LTC/USD",  "BCH/USD", "ETH/BTC",
+            //"LTC/EUR", "BCH/EUR",
+            //"US500", "VIX", "US30", "DE30", "US100", "DAX", "IT40", "SP35", "CN50", "NL25", 
+            //"Gold", "Copper"
         };
 
         public static List<string> inPositionSymbols = new List<string>
