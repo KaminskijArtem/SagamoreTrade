d52e8523a49875018be3988372a8645fac2a9afe
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-03-01 15:21:28 +0300

    1

--- QuartzScheduler/Base/Instruments.cs
+++ QuartzScheduler/Base/Instruments.cs
@@ -15,7 +15,7 @@ namespace QuartzScheduler.Base
 
         public static List<string> inPositionSymbols = new List<string>
         {
-            "US100", "ETH/EUR"
+            "ETH/EUR"
         };
     }
 }
