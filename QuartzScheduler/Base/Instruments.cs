5581c3c5d896e2f64b9a6f68ec4b103cf195a2c6
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-03-04 12:31:42 +0300

    qw

--- QuartzScheduler/Base/Instruments.cs
+++ QuartzScheduler/Base/Instruments.cs
@@ -15,7 +15,7 @@ namespace QuartzScheduler.Base
 
         public static List<string> inLongPositionSymbols = new List<string>
         {
-             "Gold", "Silver", "US100"
+            
         };
 
         public static List<string> inShortPositionSymbols = new List<string>
