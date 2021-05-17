3bf71de625b8ec8036563660e6c9373f636b2fbf
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-05-17 22:46:07 +0300

    qw

--- TradingDataLibrary/ApiClient/PositionsApiClient.cs
+++ TradingDataLibrary/ApiClient/PositionsApiClient.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -28,10 +28,10 @@ namespace TradingDataLibrary.ApiClient
             var client = new HttpClient();
             var time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
-            var sign = CreateSignature($"timestamp={time}", secretKey);
+            var sign = CreateSignature($"timestamp={time}&recvWindow=30000", secretKey);
             var request = new HttpRequestMessage()
             {
-                RequestUri = new Uri($"https://api-adapter.backend.currency.com/api/v1/tradingPositions?timestamp={time}&signature={sign}"),
+                RequestUri = new Uri($"https://api-adapter.backend.currency.com/api/v1/tradingPositions?timestamp={time}&recvWindow=30000&signature={sign}"),
                 Method = HttpMethod.Get,
             };
             request.Headers.Add("X-MBX-APIKEY", key);
@@ -66,11 +66,11 @@ namespace TradingDataLibrary.ApiClient
             var client = new HttpClient();
             var time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
-            var sign = CreateSignature($"timestamp={time}&positionId={id}", secretKey);
+            var sign = CreateSignature($"timestamp={time}&recvWindow=30000&positionId={id}", secretKey);
             var request = new HttpRequestMessage()
             {
                 RequestUri = 
-                new Uri($"https://api-adapter.backend.currency.com/api/v1/closeTradingPosition?timestamp={time}&positionId={id}&signature={sign}"),
+                new Uri($"https://api-adapter.backend.currency.com/api/v1/closeTradingPosition?timestamp={time}&recvWindow=30000&positionId={id}&signature={sign}"),
                 Method = HttpMethod.Post,
             };
             request.Headers.Add("X-MBX-APIKEY", key);
@@ -81,7 +81,23 @@ namespace TradingDataLibrary.ApiClient
 
         public async Task<bool> OpenPosition(Instrument instrument, bool isLong)
         {
-            return false;
+            var client = new HttpClient();
+            var time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            var side = isLong ? "BUY" : "SELL";
+
+            var queryString = $"timestamp={time}&recvWindow=30000&symbol={instrument.OpenPositionSymbol}&side={side}&type=MARKET&quantity={instrument.PositionSize}&leverage={instrument.Margin}&accountId=5229702511416516";
+            var sign = CreateSignature(queryString, secretKey);
+            var request = new HttpRequestMessage()
+            {
+                RequestUri =
+                new Uri($"https://api-adapter.backend.currency.com/api/v1/order?{queryString}&signature={sign}"),
+                Method = HttpMethod.Post,
+            };
+            request.Headers.Add("X-MBX-APIKEY", key);
+
+            var response = await client.SendAsync(request);
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
--- TradingDataLibrary/Models/Instruments.cs
+++ TradingDataLibrary/Models/Instruments.cs
@@ -9,54 +9,63 @@ namespace TradingDataLibrary.Models
             new Instrument
             {
                 Symbol = "BTC/USD",
+                OpenPositionSymbol = "BTC%2FUSD_LEVERAGE",
                 Margin = 5,
                 PositionSize = 0.002
             },
             new Instrument
             {
                 Symbol = "ETH/USD",
+                OpenPositionSymbol = "ETH%2FUSD_LEVERAGE",
                 Margin = 5,
                 PositionSize = 0.03
             },
             new Instrument
             {
                 Symbol = "LTC/USD",
+                OpenPositionSymbol = "LTC%2FUSD_LEVERAGE",
                 Margin = 5,
                 PositionSize = 0.35
             },
             new Instrument
             {
                 Symbol = "BCH/USD",
+                OpenPositionSymbol = "BCH%2FUSD_LEVERAGE",
                 Margin = 5,
                 PositionSize = 0.09
             },
             new Instrument
             {
                 Symbol = "ETH/BTC",
+                OpenPositionSymbol = "ETH%2FUSD_LEVERAGE",
                 Margin = 5,
                 PositionSize = 0.03
             },
             new Instrument
             {
                 Symbol = "US500",
+                OpenPositionSymbol = "US500.",
                 Margin = 20,
                 PositionSize = 0.1
             },
             new Instrument
             {
                 Symbol = "US30",
+                OpenPositionSymbol = "US30.",
                 Margin = 20,
                 PositionSize = 0.01
             },
             new Instrument
             {
                 Symbol = "US100",
+                OpenPositionSymbol = "US100.",
                 Margin = 20,
                 PositionSize = 0.03
             },
             new Instrument
             {
                 Symbol = "Gold",
+                OpenPositionSymbol = "Gold.",
                 Margin = 25,
                 PositionSize = 0.3
             }
@@ -67,6 +76,7 @@ namespace TradingDataLibrary.Models
     {
         public string Symbol { get; set; }
         public int Margin { get; set; }
-        public double PositionSize { get; internal set; }
+        public double PositionSize { get; set; }
+        public string OpenPositionSymbol { get; set; }
     }
 }
