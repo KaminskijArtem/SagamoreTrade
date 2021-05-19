98e0f3ff9b1862d4b6356794c6085d846485704a
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-05-20 00:05:45 +0300

    qw

--- TradingDataLibrary/Implementations/RSITradeCandlesService.cs
+++ TradingDataLibrary/Implementations/RSITradeCandlesService.cs
@@ -86,7 +86,7 @@ namespace TradingDataLibrary.Implementations
             var rsiList = CalculateRSI(candles);
             var rsi = rsiList.Last().Value;
 
-            if ((rsi > 55 || rsi < 35) && !position.IsLong() || (rsi < 45 || rsi > 65) && position.IsLong())
+            if ((rsi > 55 || rsi < 40) && !position.IsLong() || (rsi < 45 || rsi > 60) && position.IsLong())
             {
                 var outputModel = new InPositionRSISignalModel
                 {
