9c25f0cf7910873aa6bc6745d536513f0433796f
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-05-20 18:37:24 +0300

    qw

--- TradingDataLibrary/Implementations/RSITradeCandlesService.cs
+++ TradingDataLibrary/Implementations/RSITradeCandlesService.cs
@@ -58,7 +58,7 @@ namespace TradingDataLibrary.Implementations
                 var macdSerie = MACD.Calculate();
                 var lastMACDSignal = macdSerie.MACDHistogram.Last();
 
-                if (true)//(lastADX > 25)
+                if (lastADX > 25)
                 {
                     if (rsi > 50 && lastMACDSignal > 0)
                     {
