ddbef7a6e46bd23bda5a2bf1df30c36b51fd14f4
artsem.kaminski <artsem.kaminski@leverx-group.com>
2021-06-28 10:53:32 +0300

    qw

--- TradingDataLibrary/Models/Instruments.cs
+++ TradingDataLibrary/Models/Instruments.cs
@@ -28,7 +28,7 @@ namespace TradingDataLibrary.Models
                 OpenPositionSymbol = "US500.",
                 Margin = 20,
                 PositionSize = 0.1,
-                Strategy = Strategy.Agressive
+                Strategy = Strategy.Peaceful
             },
             new Instrument
             {
@@ -36,7 +36,7 @@ namespace TradingDataLibrary.Models
                 OpenPositionSymbol = "US30.",
                 Margin = 20,
                 PositionSize = 0.01,
-                Strategy = Strategy.Agressive
+                Strategy = Strategy.Peaceful
             },
             new Instrument
             {
